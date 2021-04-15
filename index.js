f52e00a41793bb233360b45d278a01a6a034ad69
lancetoledo <lanceadrian13@gmail.com>
2021-04-14 21:33:40 -0400

    1 file modified

--- index.js
+++ index.js
@@ -7,10 +7,17 @@ function diff(array1, array2) {
   // are only in the first array.
   let newArr = [];
   for (let i = 0; i < array1.length; i++) {
-    if (secondArr.includes(firstArr[i])) {
+    if (array2.includes(array1[i])) {
       // console.log(firstArr[i])
     } else {
-      newArr.push(firstArr[i]);
+      newArr.push(array1[i]);
+    }
+  }
+  for (let i = 0; i < array2.length; i++) {
+    if (array1.includes(array2[i])) {
+      // console.log(firstArr[i])
+    } else {
+      newArr.push(array2[i]);
     }
   }
   return newArr;
